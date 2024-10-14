c0de66ddd0092abaf03c4eb402c636821e2932ef
Cao Meng <471695442@qq.com>
2024-10-14 16:26:43 +0800

    sprite player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,12 @@ using DG.Tweening;
 public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; }
-    private readonly float speed = 5;
+    private readonly float speed = 3;
     private readonly float range = 10;
     private bool isMove = false;
     private Vector2 inputDirect = Vector2.zero;
     public Vector2 faceDirect = Vector2.up;
+    public Animator animator;
 
     private readonly Collider2D[] enemyResults = new Collider2D[10];
     private GameObject target;
@@ -48,7 +49,7 @@ public class Player : MonoBehaviour
         if (target != null)
         {
             faceDirect = (target.transform.position - transform.position).normalized;
-            transform.up = faceDirect;
+            //transform.up = faceDirect;
         }
     }
 
@@ -59,11 +60,20 @@ public class Player : MonoBehaviour
         {
             isMove = true;
             faceDirect = inputDirect;
+            if (inputDirect.x > 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (inputDirect.x < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
         }
         else
         {
             isMove = false;
         }
+        animator.SetBool("isMove", isMove);
     }
 
     void GetTarget()
